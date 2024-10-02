b8ef57e89755904039081e042c0a0dc7b2fdd757
2220901 <2220901h@cloud.kobe-u.jp>
2024-10-02 14:09:33 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -3,13 +3,14 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  strokeWeight(4);
+  background(255);
+  strokeWeight(0);
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  strokeWeight(1);
   fill(255);
+  triangle(0,108,106,108,106,0);
+  fill(250, 182, 229);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
